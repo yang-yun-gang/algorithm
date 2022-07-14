63246ecf3af9b2a8f2637bb11b5bf08a4fac53a3
yangyungang <yungang_yang@163.com>
2022-07-14 09:48:02 +0800

    [add] 最小的 k 个数 数据流中的中位数

---
+++ src/main/java/com/example/algorithm/sort/Test01.java
@@ -0,0 +1,39 @@
+package com.example.algorithm.sort;
+
+/**
+ * @Description : 剑指 Offer 40. 最小的 k 个数
+ * @Author : young
+ * @Date : 2022-07-14 8:28
+ * @Version : 1.0
+ **/
+public class Test01 {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        // 冒泡排序
+        for (int i = 0; i < k; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr[i];
+        }
+
+        return res;
+    }
+
+    private void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,1,2,1};
+        Test01 test = new Test01();
+        System.out.println(test.getLeastNumbers(arr, 1));
+    }
+}
---
+++ src/main/java/com/example/algorithm/sort/Test02.java
@@ -0,0 +1,48 @@
+package com.example.algorithm.sort;
+
+import java.util.PriorityQueue;
+
+/**
+ * @Description : 剑指 Offer 41. 数据流中的中位数
+ * @Author : young
+ * @Date : 2022-07-14 8:52
+ * @Version : 1.0
+ **/
+public class Test02 {
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian());
+    }
+}
+
+class MedianFinder {
+
+    // 小顶堆
+    PriorityQueue<Integer> minHeap;
+
+    // 大顶堆
+    PriorityQueue<Integer> maxHeap;
+
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((o1, o2)->o2 - o1);
+    }
+
+    public void addNum(int num) {
+        if (minHeap.size() == maxHeap.size()) {
+            minHeap.add(num);
+            maxHeap.add(minHeap.poll());
+        } else {
+            maxHeap.add(num);
+            minHeap.add(maxHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        return (maxHeap.size() == minHeap.size()) ? (maxHeap.peek() + minHeap.peek()) / 2.0 : maxHeap.peek();
+    }
+}
